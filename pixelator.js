ddb795a149bd07537100f910b50c52a69bc60eed
SeanCruise101 <84522434+SeanCruise101@users.noreply.github.com>
2021-07-14 11:46:47 -0400

    Add files via upload

---
+++ pixelator.js
@@ -0,0 +1,202 @@
+class Pixelator {
+
+    /**
+     * Creates the object
+     * @param  {ImageData} imageData   an ImageData object. Can be "real" or "fabricated" (meaning it
+     *                                 simply matches the interface of ImageData)
+     * @param  {int} spWidth           Super Pixel Width
+     * @param  {int} spHeight          Super Pixel Height
+     * @param  {int} numSpx            Number of Super Pixels on the X axis
+     * @param  {int} numSpy            Number of Super Pixels on the Y axis
+     * @param  {bool} hideTheGrid      true hides the "grid" overlay, false (or unset) shows it.
+     */
+    constructor(imageData, chunkStartY, spWidth, spHeight, numSpx, numSpy, hideTheGrid = false){
+      this._bitPacker = new BitPacker();
+  
+      // Dump params to this
+      this._spWidth = spWidth;
+      this._spHeight = spHeight;
+      this._numSpx = numSpx;
+      this._numSpy = numSpy;
+      this._hideTheGrid = hideTheGrid;
+  
+      // Dump imageData into it's parts
+      this._imageData = imageData;
+      this._imageWidth = imageData.width;
+      this._imageHeight = imageData.height;
+  
+      // Create a uint32 view of the imageData buffer
+      this._uInt32Array = new Uint32Array(this._imageData.data.buffer);
+  
+      // Cache the packed grid pixel color for speed
+      this._gridPixelColorPacked = this._bitPacker.packPixel(50, 50, 50, 255);
+  
+      // This is the number of spy's that came before this chunk (for the grid every x pixels code)
+      this._prevSpy = (chunkStartY / this._spHeight) | 0;
+    }
+  
+    /**
+     * Runs the Pixelator algorithm to actually convert the image into a pattern
+     *
+     * @return {Promise} resolve(pixelatedImageData)
+     */
+    run(){
+      for(let spx = 0; spx < this._numSpx; spx++){
+        for(let spy = 0; spy < this._numSpy; spy++){
+          let color = this._getSuperPixelColor(spx, spy);
+          this._setSuperPixelColor(spx, spy, color);
+        }
+      }
+      return this._imageData;
+    }
+  
+  
+  
+    /**
+     * Gets the super pixels' "overall" color.
+     * Currently this works by just taking the mode of the entire super-pixel and returning that
+     *
+     * @param  {int} spx    Super Pixel X Pos
+     * @param  {int} spy    Super Pixel Y Pos
+     * @return {Array}      [red, green, blue, alpha] pixel color
+     */
+    _getSuperPixelColor(spx, spy){
+      let map = {};
+      let mode = null;
+      let largestCount = 1;
+  
+      for(let pixelX = 0; pixelX < this._spWidth; pixelX++){
+        for(let pixelY = 0; pixelY < this._spHeight; pixelY++){
+          let [xPos, yPos] = this._getPixelPos(spx, spy, pixelX, pixelY);
+  
+          // Ignore anything outside of the image bounds
+          if(xPos < this._imageWidth && yPos < this._imageHeight){
+            let pixelModeNumber = 1;
+  
+            // Get the actual pixel value
+            let pixel = this._uInt32Array[(yPos * this._imageWidth) + xPos];
+  
+            // If this is the first pixel, then it's the mode to start with!
+            if(pixelX === 0 && pixelY === 0){
+              mode = pixel;
+            }
+  
+            if(pixel in map){
+              pixelModeNumber = map[pixel] + 1;
+            }
+  
+            map[pixel] = pixelModeNumber;
+  
+            if(pixelModeNumber > largestCount){
+              mode = pixel;
+              largestCount = pixelModeNumber;
+            }
+          }
+        }
+      }
+  
+      if(mode === null){
+        return [0, 0, 0, 0];
+      }else{
+        return this._bitPacker.unpackPixel(mode);
+      }
+    }
+  
+  
+    /**
+     * Gets the super pixels' "overall" color.
+     * This is a "fast" version which just uses the center of each "super pixel" as the main color
+     *
+     * @param  {int} spx    Super Pixel X Pos
+     * @param  {int} spy    Super Pixel Y Pos
+     * @return {Array}      [red, green, blue, alpha] pixel color
+     */
+    _fastGetSuperPixelColor(spx, spy){
+      let [xPos, yPos] = this._getPixelPos(spx, spy, (this._spWidth / 2) | 0, (this._spHeight / 2) | 0);
+  
+      let pixel = this._uInt32Array[(yPos * this._imageWidth) + xPos];
+  
+      return this._bitPacker.unpackPixel(pixel);
+    }
+  
+    /**
+     * Sets the given super pixel to the given color
+     *
+     * @param  {int} spx    Super Pixel X Pos
+     * @param  {int} spy    Super Pixel Y Pos
+     */
+    _setSuperPixelColor(spx, spy, color){
+      let [red, green, blue, alpha] = color;
+      let trueSpy = this._prevSpy + spy;
+  
+      for(let pixelX = 0; pixelX < this._spWidth; pixelX++){
+        for(let pixelY = 0; pixelY < this._spHeight; pixelY++){
+          let [xPos, yPos] = this._getPixelPos(spx, spy, pixelX, pixelY);
+  
+          // Ignore anything outside of the image bounds
+          if(xPos < this._imageWidth && yPos < this._imageHeight){
+            let index = ((yPos * this._imageWidth) + xPos) | 0;
+  
+  
+            // if this pixel is mostly alpha, then just convert it to all white
+            if(alpha < 200){
+              red = blue = green = 255;
+            }
+  
+            this._uInt32Array[index] = this._getPixelColorOrGrid(pixelX, pixelY, spx, trueSpy, red, green, blue);
+          }
+        }
+      }
+    }
+  
+    _getPixelColorOrGrid(pixelX, pixelY, spx, spy, red, green, blue){
+  
+      if(!this._hideTheGrid){
+        let gridPixelColorPacked = this._gridPixelColorPacked;
+        let internalSpWidth = this._spWidth - 1;
+        let internalSpHeight = this._spHeight - 1;
+        if((spx + 1) % 10 === 0){
+          // if we get here, we want a fat line
+          if(pixelX === internalSpWidth || pixelX === internalSpWidth - 1){
+            return gridPixelColorPacked;
+          }
+        }else if(pixelX === internalSpWidth){
+          // else we want a normal thickness line
+          return gridPixelColorPacked;
+        }
+  
+        if((spy + 1) % 10 === 0){
+          // if we get here, we want a fat line
+          if(pixelY === internalSpHeight || pixelY === internalSpHeight - 1){
+            return gridPixelColorPacked;
+          }
+        }else if(pixelY === internalSpHeight){
+          // else we want a normal thickness line
+          return gridPixelColorPacked;
+        }
+  
+        // if we are the first spx or spy, then make a line on the left and top as well
+        if((spx === 0  && pixelX === 0) || (spy === 0 && pixelY === 0)){
+          return gridPixelColorPacked;
+        }
+      }
+  
+      return this._bitPacker.packPixel(red, green, blue, 255);
+    }
+  
+    /**
+     * Gets the global postion of an individual pixel from the x/y super pixel and sub-pixel x/y
+     *
+     * @param  {int} spx    Super Pixel X Pos
+     * @param  {int} spy    Super Pixel Y Pos
+     * @param  {int} pixelX Sub Pixel X Pos
+     * @param  {int} pixelY Sub Pixel X Pos
+     * @return {Array}      [xPos, yPos] position of the individual pixel
+     */
+    _getPixelPos(spx, spy, pixelX, pixelY){
+      let xPos = (spx * this._spWidth) + pixelX;
+      let yPos = (spy * this._spHeight) + pixelY;
+  
+      return [xPos, yPos];
+    }
+  }
---
+++ quantizor.js
@@ -0,0 +1,109 @@
+class Quantizor {
+
+    /**
+     * Creates the object
+     *
+     * @param  {ImageData} imageData    Can be "real" or "created" in the sense that it
+     *                                  simply follows the real ImageData interface.
+     * @param  {Bool} useDmcColors      True if they should be used, false or undefined otherwise
+     * @param  {Bool} highQualityMode   True if high quality mode, false or undefined otherwise
+     */
+    constructor(imageData, useDmcColors = false, highQualityMode = false){
+      this._useDmcColors = useDmcColors;
+      this._imageData = imageData;
+      this._highQualityMode = highQualityMode;
+      this._dmcColorsObj = new Colorizor();
+    }
+  
+    /**
+     * Builds and returns the palette of colors in the image
+     *
+     * @param  {int} numColors Number of colors to try and reduce the palette to
+     * @return {Promise}       resolve(palette) palette is an array of [r,g,b] tuples
+     */
+    buildPalette(numColors){
+      let rgbq = new RgbQuant(this._buildOpts({numColors}));
+      rgbq.sample(this._imageData);
+      let palette = rgbq.palette(true);
+  
+      return palette;
+    }
+  
+    /**
+     * Actually quantizes the image to reduce it's colors to only the palette colors
+     *
+     * @param  {Array} palette An array of [r,g,b] tuples of 0-255 each for each color
+     * @return {Promise}       resolve(quantizedImageData) the quantized image data (in "fake"
+     *                         imageData form in that it just follows the real ImageData interface)
+     */
+    quantize(palette){
+      let rgbq = new RgbQuant(this._buildOpts({palette}));
+      let quantizedUint8Array = rgbq.reduce(this._imageData, 1);
+  
+      let returnImageData = {
+        data: new Uint8ClampedArray(quantizedUint8Array),
+        width: this._imageData.width,
+        height: this._imageData.height
+      };
+  
+      return returnImageData;
+    }
+  
+    /**
+     * creates the options object to be passed to RgbQuant.js
+     * @param  {[int]} numColors     [the number of colors to use (optional)]
+     * @param  {[array]} palette     [the palette to be used (optional)]
+     * @return {[Object]}            [the hash to put into RgbQuant.js]
+     */
+    _buildOpts({numColors, palette}){
+      let opts = {
+        colorDist: 'euclidean',
+        useCache: false,
+        colors: numColors,
+        reIndex: true,
+        palette: this._getPalette(palette)
+      };
+  
+      if(this._highQualityMode){
+        opts.method = 2;
+        opts.boxSize = [32,32];
+        opts.boxPxls = 2;
+        opts.minHueCols = 512;
+        opts.dithKern = 'FloydSteinberg';
+        opts.dithDelt = 0.1;
+      }else{
+        opts.method = 1;
+        opts.initColors = this._getInitColors();
+      }
+  
+      return opts;
+    }
+  
+    /**
+     * gets the number of init colors depending on if DMC colors are used
+     *
+     * @return {int} either 2048 or the number of DMC colors available
+     */
+    _getInitColors(){
+      if(this._useDmcColors){
+        return this._dmcColorsObj.getDmcColorMap().size;
+      }else{
+        return 2048;
+      }
+    }
+  
+    /**
+     * returns the palette
+     * @param  {[array]} palette [the palette if applicable]
+     * @return {[multiple]}         [the palette to pass into the RgbQuant.js object]
+     */
+    _getPalette(palette = false){
+      if(palette !== false){
+        return palette;
+      }else if(this._useDmcColors){
+        return this._dmcColorsObj.getColorsAsRGB();
+      }else{
+        return undefined;
+      }
+    }
+  }
